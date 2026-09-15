520b6943ae90b16f8db98c5714bdd9028c1108ae
Pilou44 <guillaume.be@cheerz.com>
2026-09-15 18:10:15 +0200

    Updat file hierarchy

--- src/compositor/Screen.h
+++ src/compositor/Screen.h
@@ -20,15 +20,15 @@ struct Screen {
         int pixel_y = y % 8;
 
         TilemapEntry entry_b = plane_b.entry(x, y);
-        uint16_t pixel_b = tiles[entry_b.tile_index()].pixel(pixel_x, pixel_y, entry_b.flip_h(), entry_b.flip_v());
+        uint16_t pixel_b = tiles[entry_b.palette_line()].pixel(pixel_x, pixel_y, entry_b.flip_h(), entry_b.flip_v());
         if (pixel_b != 0) {
-            color = palettes[entry_b.palette_index()].colors[pixel_b - 1];
+            color = palettes[entry_b.palette_line()].colors[pixel_b - 1];
         }
 
         TilemapEntry entry_a = plane_a.entry(x, y);
-        uint16_t pixel_a = tiles[entry_a.tile_index()].pixel(pixel_x, pixel_y, entry_a.flip_h(), entry_a.flip_v());
+        uint16_t pixel_a = tiles[entry_a.palette_line()].pixel(pixel_x, pixel_y, entry_a.flip_h(), entry_a.flip_v());
         if (pixel_a != 0) {
-            color = palettes[entry_a.palette_index()].colors[pixel_a - 1];
+            color = palettes[entry_a.palette_line()].colors[pixel_a - 1];
         }   
         return color;
     }
